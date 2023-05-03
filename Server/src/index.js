acaeddbf6ee7a7f1015eb01c0070f3800a634031
MaxChertudi <126177692+MaxChertudi@users.noreply.github.com>
2023-05-03 13:58:37 -0300

    2

--- Server/src/index.js
+++ Server/src/index.js
@@ -5,12 +5,7 @@ var http = require("http");
 http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
     
-  /* Remove id from URL */
-  let arrUrl = req.url.split('/');
-  arrUrl.pop();
-  let url = arrUrl.join('/')
-
-  if (url === '/rickandmorty/character') {
+  if (req.url.includes('/rickandmorty/character')) { 
     /* get ID from URL */
     let arrUrl = req.url.split('/');
     let id = Number(arrUrl[arrUrl.length - 1]);
@@ -18,7 +13,6 @@ http.createServer((req, res) => {
     /* find the character */
     let data = jsonData.find((item) => id===item.id);
   
-    console.log( "data;",data);
     res.writeHead(200, {'Content-type':'application/json'});
     res.end(JSON.stringify(data));
   }
