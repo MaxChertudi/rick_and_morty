ab0cf6918f657998be41af4342a934134e8de10f
MaxChertudi <126177692+MaxChertudi@users.noreply.github.com>
2023-05-05 15:06:32 -0300

    7

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -1,20 +1,23 @@
 const axios = require('axios');
 
 getCharById = (res, id) => {
+    
     axios(`https://rickandmortyapi.com/api/character/${id}`)
-        .then(({ data }) => {
+        .then((response) => response.data)
+        .then((data) => {
             const returnedObj = {
                 id: id,
                 name: data.name,
                 gender: data.gender,
                 species: data.species,
-                origin: data.origin,                    image: data.image,
+                origin: data.origin,                    
+                image: data.image,
                 status: data.status};
             res.writeHead(200, {'Content-type':'application/json'});
             res.end(JSON.stringify(returnedObj));
         })
         .catch((error) => {
-            res.writeHead(200, {'Content-type':'text/plain'});
+            res.writeHead(500, {'Content-type':'text/plain'});
             res.end(error.message);
         })
     return;
